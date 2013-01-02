16f5984fd306af46d18c6fb5b9248e27744d3893
Eugene <yeo.eugene.oey@gmail.com>
2013-01-02 08:36:05 +0800

    Search API through the system

--- stack/api.py
+++ stack/api.py
@@ -1,7 +1,10 @@
 from tastypie.resources import Resource, ModelResource, ALL
 from tastypie import fields
+from tastypie.utils import trailing_slash
 from stack.models import App, AppStack, Architecture
 from django.conf.urls import url
+from django.core.paginator import Paginator, InvalidPage
+from haystack.query import SearchQuerySet
 
 class SystemResource(ModelResource):
     class Meta:
@@ -10,11 +13,47 @@ class SystemResource(ModelResource):
         fields = ['name', 'description', 'slug', 'modified_at','logo']
         include_resource_uri = False
 
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/search%s$" % (
+                    self._meta.resource_name, trailing_slash()),
+                self.wrap_view('get_search'),
+                name="api_get_search"),
+        ]
+
+    def get_search(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        self.is_authenticated(request)
+        self.throttle_check(request)
+
+        sqs = SearchQuerySet().models(App).load_all().auto_query(request.GET.get('q',''))
+        paginator = Paginator(sqs, 20)
+
+        try:
+            page = paginator.page(int(request.GET.get('page', 1)))
+        except InvalidPage:
+            raise Http404("No Result Found")
+
+        objects = []
+        for result in page.object_list:
+            bundle = self.build_bundle(obj=result.object, request=request)
+            bundle = self.full_dehydrate(bundle)
+            objects.append(bundle)
+
+        object_list = {
+            'objects': objects,
+        }
+
+        self.log_throttled_access(request)
+        return self.create_response(request, object_list)
+
+
 class ArchitectureResource(ModelResource):
     references = fields.ToManyField('stack.api.ReferenceResource', 'references', null=True, full=True)
 
     class Meta:
         queryset = Architecture.objects.all()
+
 
 class AppResource(ModelResource):
     stacks = fields.ToManyField('stack.api.AppStackResource',
@@ -27,10 +66,12 @@ class AppResource(ModelResource):
 
     def prepend_urls(self):
         return [
-            url(r"^(?P<resource_name>%s)/(?P<slug>[\w\d_.-]+)/$" %\
-                    self._meta.resource_name,
-                    self.wrap_view('dispatch_detail'),
-                    name="api_dispatch_detail"),]
+            url(r"^(?P<resource_name>%s)/(?P<slug>[\w\d_.-]+)%s$" % (
+                    self._meta.resource_name, trailing_slash()),
+                self.wrap_view('dispatch_detail'),
+                name="api_dispatch_detail"),
+        ]
+
 
 class AppStackResource(ModelResource):
     stack = fields.ToOneField(AppResource, 'stack', full=True, max_depth=2)
@@ -38,6 +79,7 @@ class AppStackResource(ModelResource):
 
     class Meta:
         queryset = AppStack.objects.all()
+
 
 class StackResource(ModelResource):
     stacks = fields.ToManyField('self',
@@ -48,6 +90,7 @@ class StackResource(ModelResource):
 
 from tastypie.contrib.contenttypes.fields import GenericForeignKeyField
 from ref.models import Reference
+
 
 class ReferenceResource(ModelResource):
 
