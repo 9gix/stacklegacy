8bdc2de97b2fb0d0af3aed7e87b554b0d87609d5
Eugene <yeo.eugene.oey@gmail.com>
2013-01-06 08:04:41 +0800

    Browse systems by recently updated and latest system

--- sl/static/js/controllers.js
+++ sl/static/js/controllers.js
@@ -11,6 +11,27 @@ function SystemListCtrl($scope, $location){
 
 }
 
+function HomeCtrl($scope, $http){
+    $http.get('stack/api/v1/system/?format=json'
+            ).success(function(result){
+                $scope.systems = result;
+            });
+}
+
+function UpdatedSystemListCtrl($scope, $http){
+    $http.get('stack/api/v1/system/?format=json&order_by=-modified_at'
+            ).success(function(result){
+                $scope.systems = result;
+            });
+}
+
+function LatestSystemListCtrl($scope, $http){
+    $http.get('stack/api/v1/system/?format=json&order_by=-created_at'
+            ).success(function(result){
+                $scope.systems = result;
+            });
+}
+
 function SystemSearchCtrl($scope, $resource, $routeParams){
     $scope.query = $routeParams.q;
     $scope.sl = $resource('stack/api/v1/system/search/', {format: 'json', q: ''});
